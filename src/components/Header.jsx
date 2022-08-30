c241fc7d076ce9d90ac534bc71af1f5a563511ad
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-08-30 19:13:31 -0300

    [REQ-04] Create navigation links in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Loading from './Loading';
 import { getUser } from '../services/userAPI';
 
@@ -18,6 +19,9 @@ export default class Header extends Component {
     return (
       <header data-testid="header-component">
         <h1>TrybeTunes</h1>
+        <Link data-testid="link-to-search" to="/search">Search</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        <Link data-testid="link-to-profile" to="/profile">Profile</Link>
         {loading ? <Loading /> : <h2 data-testid="header-user-name">{userName}</h2>}
       </header>
     );
