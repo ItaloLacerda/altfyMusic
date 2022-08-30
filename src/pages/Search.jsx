3300064389027460ed7653ad8b9133cbc65f6149
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-08-30 19:46:59 -0300

    [REQ-05] Create the form to search for artists

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,39 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    searchEntry: '',
+  };
+
+  handelChange = ({ target }) => {
+    const { value } = target;
+    this.setState({ searchEntry: value });
+  };
+
   render() {
+    const { searchEntry } = this.state;
+    const minimumCharacters = 2;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <label htmlFor="searchEntry">
+            <input
+              data-testid="search-artist-input"
+              id="searchEntry"
+              type="text"
+              onChange={ this.handelChange }
+              value={ searchEntry }
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ searchEntry.length < minimumCharacters }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
