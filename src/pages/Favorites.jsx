6263f7c0c9c8478d57c70da9360f40491a0aa906
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-09-01 05:39:05 -0300

    [REQ-12] Render list of favorites in favorite file

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 export default class Favorites extends Component {
+  state = {
+    loading: false,
+    favoriteSongs: [],
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const favoriteSongs = await getFavoriteSongs();
+    this.setState({ loading: false, favoriteSongs });
+  }
+
   render() {
+    const { loading, favoriteSongs } = this.state;
     return (
-      <div data-testid="page-favorites">
-        <Header />
-        Favorites
-      </div>
+      loading ? <Loading /> : (
+        <div data-testid="page-favorites">
+          <Header />
+          {favoriteSongs.map((music) => (<MusicCard
+            favoriteSongs={ favoriteSongs }
+            track={ music }
+            trackId={ music.trackId }
+            key={ music.trackId }
+            trackName={ music.trackName }
+            previewUrl={ music.previewUrl }
+          />))}
+        </div>)
     );
   }
 }
